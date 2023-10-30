1366374a15a6cbfdf666f933058bb07d5bd1fd16
KevanAwes <kevanawesome@gmail.com>
2023-10-30 08:45:00 +0800

    Lab3 BMI return

--- Exercise1/BMI.py
+++ Exercise1/BMI.py
@@ -7,9 +7,12 @@ def calculate_bmi(height, weight):
     print("Weight Classification:")
     if bmi < 18.5:
         print("Under Weight")
+        return -1
     elif bmi >= 18.5 and bmi <= 25.0:
         print("Normal Weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
